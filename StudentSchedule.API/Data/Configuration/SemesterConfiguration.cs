a8787788d389f14a61962df1c04acde971d4a6ab
aaroniz-bgu <aaroni@post.bgu.ac.il>
2023-09-27 00:12:05 +0300

    configured

---
+++ StudentSchedule.API/Data/Configuration/SemesterConfiguration.cs
@@ -0,0 +1,34 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using StudentSchedule.API.Domain.Models;
+
+namespace StudentSchedule.API.Data.Configuration;
+
+public class SemesterConfiguration : IEntityTypeConfiguration<Semester>
+{
+    public void Configure(EntityTypeBuilder<Semester> builder)
+    {
+        builder
+            .HasMany(s => s.Courses)
+            .WithOne(c => c.Semester)
+            .HasForeignKey(s => s.Id)
+            .IsRequired();
+
+        builder.HasKey(x => x.Id);
+        
+        builder
+            .Property(x=>x.Id)
+            .IsRequired()
+            .IsUnicode()
+            .ValueGeneratedOnAdd();
+        builder
+            .Property(x => x.Title)
+            .IsRequired();
+        builder
+            .Property(x => x.StartDate)
+            .IsRequired();
+        builder
+            .Property(x => x.EndDate)
+            .IsRequired();
+    }
+}
