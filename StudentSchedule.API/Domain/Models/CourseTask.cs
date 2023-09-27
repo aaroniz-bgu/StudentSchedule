37a06445d597f3dddb535116fa2c3a151809706d
aaroniz-bgu <aaroni@post.bgu.ac.il>
2023-09-27 22:18:40 +0300

    Additional constructor, and doc

--- StudentSchedule.API/Domain/Models/CourseTask.cs
+++ StudentSchedule.API/Domain/Models/CourseTask.cs
@@ -9,6 +9,15 @@ public class CourseTask : Progressable
     public DateTime DueDate { get; set; } //TODO Check legality of changing DueDate in service.
     public Course Course { get; private set; }
     
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="id">Course's identifier key.</param>
+    /// <param name="title">Course's title, cannot be null or empty.</param>
+    /// <param name="description">Course's description, cannot be null or empty.</param>
+    /// <param name="dueDate">Task's deadline.</param>
+    /// <param name="type">Look at TaskType enum for more details.<seealso cref="TaskType"/></param>
+    /// <param name="course">The course that this task was given at.</param>
     public CourseTask(
         long id,
         string title, string description, 
@@ -17,6 +26,51 @@ public class CourseTask : Progressable
     {
         Id = id;
         
+        Title = title;
+        Description = description;
+        
+        DueDate = dueDate;
+        
+        Course = course;
+        Type = type;
+    }
+    
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="id">Course's identifier key.</param>
+    /// <param name="title">Course's title, cannot be null or empty.</param>
+    /// <param name="description">Course's description, cannot be null or empty.</param>
+    /// <param name="dueDate">Task's deadline.</param>
+    /// <param name="type">Look at TaskType enum for more details.<seealso cref="TaskType"/></param>
+    public CourseTask(
+        long id,
+        string title, string description, 
+        DateTime dueDate, 
+        TaskType type)
+    {
+        Id = id;
+        
+        Title = title;
+        Description = description;
+        
+        DueDate = dueDate;
+        
+        Course = null;
+        Type = type;
+    }
+    
+    /// <summary></summary>
+    /// <param name="title">Course's title, cannot be null or empty.</param>
+    /// <param name="description">Course's description, cannot be null or empty.</param>
+    /// <param name="dueDate">Task's deadline.</param>
+    /// <param name="type">Look at TaskType enum for more details.<seealso cref="TaskType"/></param>
+    /// <param name="course">The course that this task was given at.</param>
+    public CourseTask(
+        string title, string description, 
+        DateTime dueDate, 
+        TaskType type, Course course)
+    {
         Title = title;
         Description = description;
         
