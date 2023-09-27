a58177369ae5e4dc856fc4b9a5e3076e67f6b868
aaroniz-bgu <aaroni@post.bgu.ac.il>
2023-09-27 16:50:40 +0300

    second constructor for inner exception

--- StudentSchedule.API/Exception/AppException.cs
+++ StudentSchedule.API/Exception/AppException.cs
@@ -2,9 +2,14 @@ namespace StudentSchedule.API.Exception;
 
 public class AppException : System.Exception
 {
-    public int StatusCode { get; set; }
+    public int StatusCode { get; protected set; }
     
     public AppException(int statusCode, string message) : base(message)
+    {
+        StatusCode = statusCode;
+    }
+
+    public AppException(int statusCode, System.Exception exception) : base(exception.Message, exception)
     {
         StatusCode = statusCode;
     }
