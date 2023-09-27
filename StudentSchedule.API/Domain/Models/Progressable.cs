1bcbc9478b362356cec5b942104e32fbdc65a954
aaroniz-bgu <aaroni@post.bgu.ac.il>
2023-09-27 22:19:35 +0300

    deleting unnecessary code,using auto props.

--- StudentSchedule.API/Domain/Models/Progressable.cs
+++ StudentSchedule.API/Domain/Models/Progressable.cs
@@ -2,18 +2,9 @@ namespace StudentSchedule.API.Domain.Models;
 
 public abstract class Progressable
 {
-    private int _progress = 0;
-    public int Progress
-    {
-        get => _progress;
-        protected set
-        {
-            if (value > MaxProgress || value < 0) throw new ArgumentException("Progress cannot be greater than MaxProgress");
-            
-            _progress = value;
-        }
-    }
-    protected const int DefaultMaxProgress = 100;
-    protected int MaxProgress { get; } = DefaultMaxProgress;
+    private const int DefaultMaxProgress = 100;
+    public int Progress { get; internal set; } = 0;
+    //Do not add a setter yet, if you do configure that for the context.
+    public int MaxProgress { get; } = DefaultMaxProgress;
     public bool IsCompleted => Progress == MaxProgress;
 }
