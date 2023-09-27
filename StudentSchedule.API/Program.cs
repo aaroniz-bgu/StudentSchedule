4fc0c3c80040b6029367bb2dde541a5ae00a781c
aaroniz-bgu <aaroni@post.bgu.ac.il>
2023-09-27 23:37:14 +0300

    temp

--- StudentSchedule.API/Program.cs
+++ StudentSchedule.API/Program.cs
@@ -1,6 +1,43 @@
+
+using Microsoft.EntityFrameworkCore;
+using StudentSchedule.API.Data;
+using StudentSchedule.API.Services;
+using StudentSchedule.API.Services.IServices;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.Configure<RouteOptions>(options => options.LowercaseUrls = true);
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseInMemoryDatabase("StudentSchedule");
+});
+
+builder.Services.AddScoped<IServiceGatherer, ServiceGatherer>();
+builder.Services.AddScoped<ISemesterService, SemesterService>();
+builder.Services.AddScoped<ICourseService, CourseService>();
+builder.Services.AddScoped<ITaskService, TaskService>();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseRouting();
+app.UseAuthorization();
+app.UseExceptionHandler("/error");
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+});
 
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.MapControllers();
 app.Run();
