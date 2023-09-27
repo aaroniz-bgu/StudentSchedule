6e86598b0d0d3bdde22acbc5d160a1d9d4151eca
aaroniz-bgu <aaroni@post.bgu.ac.il>
2023-09-27 10:55:11 +0300

    Uneeded

--- StudentSchedule.API/Services/IServices/ISemesterService.cs
+++ StudentSchedule.API/Services/IServices/ISemesterService.cs
@@ -9,8 +9,4 @@ public interface ISemesterService
     Task<Semester> AddSemesterAsync(string title, DateTime startDate, DateTime endDate);
     Task UpdateSemesterAsync(Semester semester);
     Task DeleteSemesterAsync(long id);
-    
-    Task<List<Course>> GetCoursesAsync(long semesterId);
-    Task RemoveCourse(long semesterId, long courseId);
-    Task AddCourse(long semesterId, Course course);
 }
