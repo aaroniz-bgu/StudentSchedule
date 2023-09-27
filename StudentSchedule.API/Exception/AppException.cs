3a8fb0ad7177a674debc3a910818ce71723ca320
aaroniz-bgu <aaroni@post.bgu.ac.il>
2023-09-27 18:28:02 +0300

    doc

--- StudentSchedule.API/Exception/AppException.cs
+++ StudentSchedule.API/Exception/AppException.cs
@@ -4,11 +4,21 @@ public class AppException : System.Exception
 {
     public int StatusCode { get; protected set; }
     
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="statusCode">HTTPStatus code.</param>
+    /// <param name="message">Message describing the exception occured.</param>
     public AppException(int statusCode, string message) : base(message)
     {
         StatusCode = statusCode;
     }
 
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="statusCode">HTTPStatus code.</param>
+    /// <param name="exception">Inner exception.</param>
     public AppException(int statusCode, System.Exception exception) : base(exception.Message, exception)
     {
         StatusCode = statusCode;
