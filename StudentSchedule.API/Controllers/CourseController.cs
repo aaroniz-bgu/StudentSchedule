cee36fbbdf90314eda410d49a109472a50bc9de2
aaroniz-bgu <aaroni@post.bgu.ac.il>
2023-09-27 18:27:50 +0300

    matching the services and now using proper docs, routes, signatures and more

--- StudentSchedule.API/Controllers/CourseController.cs
+++ StudentSchedule.API/Controllers/CourseController.cs
@@ -2,6 +2,8 @@
 using Microsoft.AspNetCore.Mvc;
 using StudentSchedule.API.Domain.Models;
 using StudentSchedule.API.Services.IServices;
+using StudentSchedule.Contracts.Requests;
+using StudentSchedule.Contracts.Responses;
 
 namespace StudentSchedule.API.Controllers;
 
@@ -17,37 +19,56 @@ public class CourseController : ControllerBase
     }
     
     [HttpGet]
-    public async Task<ActionResult> GetCoursesAsync()
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public async Task<ActionResult> GetCourses()
     {
         var courses = await _service.GetCoursesAsync();
-        return Ok(courses);
+        var response = courses.Select(ConvertResponse).ToList();
+        return Ok(response);
     }
     
-    [HttpGet("{id}")]
-    public async Task<ActionResult> GetCourseAsync(long id)
+    [HttpGet("{id:long}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<ActionResult> GetCourse(long id)
     {
         var course = await _service.GetCourseAsync(id);
-        return Ok(course);
+        var response = ConvertResponse(course);
+        return Ok(response);
     }
     
     [HttpPost]
-    public async Task<ActionResult> AddCourseAsync(string title, string description, DateTime startDate, DateTime endDate)
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<ActionResult> AddCourse(CourseRequest request)
     {
-        var newCourse = await _service.AddCourseAsync(title, description, startDate, endDate);
-        return Ok(newCourse);
+        var course = await _service.AddCourseAsync(request.SemesterId, request.Title);
+        var response = ConvertResponse(course);
+        return CreatedAtAction(nameof(GetCourse), new { id = response.Id },response);
     }
     
-    [HttpPut]
-    public async Task<ActionResult> UpdateCourseAsync(Course course)
+    [HttpPut("{id:long}/{title}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<ActionResult> UpdateCourse(long id, string title)
     {
-        await _service.UpdateCourseAsync(course);
+        await _service.UpdateCourseAsync(id, title);
         return Ok();
     }
     
-    [HttpDelete("{id}")]
-    public async Task<ActionResult> DeleteCourseAsync(long id)
+    [HttpDelete("{id:long}")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<ActionResult> DeleteCourse(long id)
     {
         await _service.DeleteCourseAsync(id);
-        return Ok();
+        return NoContent();
+    }
+
+    private CourseResponse ConvertResponse(Course course)
+    {
+        return new CourseResponse(course.Semester.Id, course.Id, course.Title);
     }
 }
