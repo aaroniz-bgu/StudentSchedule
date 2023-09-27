2572faa0385fb62e89eac8c79e108cdf6c368807
aaroniz-bgu <aaroni@post.bgu.ac.il>
2023-09-27 16:51:29 +0300

    Initial commit

---
+++ StudentSchedule.API/Controllers/Exception/ExceptionController.cs
@@ -0,0 +1,42 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using StudentSchedule.API.Exception;
+
+namespace StudentSchedule.API.Controllers.Exception;
+
+[AllowAnonymous]
+[ApiExplorerSettings(IgnoreApi = true)]
+public class ExceptionController : ControllerBase
+{
+    // Credit/thanks to stackoverflow.com/users/2061604/andrei
+    [Route("/error")]
+    public IActionResult Error()
+    {
+        var context = HttpContext.Features.Get<IExceptionHandlerFeature>();
+        var exception = context!.Error;
+
+        try
+        {
+            throw exception;
+        }
+        catch (AppException ex)
+        {
+            return Problem(
+                statusCode: (int)ex.StatusCode,
+                detail: ex.Message
+            );
+        }
+        catch (ArgumentException ex)
+        {
+            return BadRequest(ex.Message);
+        }
+        catch(System.Exception ex) 
+        {
+            return Problem(
+                statusCode: 500,
+                detail: ex.Message
+            );
+        }
+    }
+}
